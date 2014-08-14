a344781e540e6f9537b7850dab48b72af5435564
Richard Lawrence <richard.lawrence@berkeley.edu>
2014-08-14 08:59:21 -0700

    * Boxes.elm: dynamically add activatable boxes
    Things I learned:
    
    The general problem I was having trouble solving was: how do I update
    a value based on *two* parameters when only *one* of them updates?
    For example, I wanted to update a list of boxes by adding a new box
    when a button was clicked.  Adding a new box required the previous
    value of the list, to compute a unique identifier for the new box.
    But it also required an incoming signal from the button.  So two
    signals need to be involved here: one representing the value of a list
    of boxes, one representing button clicks.  The problem is figuring out
    how to update the list of boxes, based on the previous value,
    when *only* the button click signal changes.
    
    lift2 does not seem to work in this scenario, I think because the
    output signal only updates when both input signals change (not sure
    about this, though...I just know I tried it and it failed).  Or at
    least, lift2-ing a function f a b whose output value only depends on
    b (boxes) does not update the value when only a new a (click) is
    given.
    
    The solution I came up with was using foldp.  With foldp, I could make
    the signal representing the list of boxes update based on both its
    previous state and the signal from the button.  This gave me an
    intermediate signal, pdBoxes, of type Signal [Box] but updating on
    button clicks.
    
    This was also the first time I found it useful to have intermediate
    signals, i.e., signals which I didn't construct directly with input
    and which I didn't use directly in main.
    
    Finally, I found I could use lift2 to combine the signal representing
    the list of boxes with the signal representing the active box to
    create the signal for the main scene.  Not so sure why lift2 seems to
    work here but didn't in my previous attempts. I think it's because, the
    way I'm using it here, the output value changes whenever the first
    input changes (since the output is just a tuple of the first and
    second inputs).  Before, I was just throwing away the first value
    because only the second value was needed to compute a new output.
    
    The code ain't so pretty, but I solved the hard problem of dynamically
    updating a list based on both its previous value and on user input,
    and it works!

--- Boxes.elm
+++ Boxes.elm
@@ -1,29 +1,67 @@
-import Graphics.Input (Input, input, clickable)
+import Graphics.Input (Input, input, button, clickable)
 
+-- Box type, constructor and accessors
 data Box = Box Int Element
 
-activeElement : Input Int
-activeElement = input 1
+getId (Box id el) = id
 
 makeBox boxId = asText ("Box " ++ show boxId)
   |> container 40 50 middle
   |> clickable activeElement.handle boxId
   |> Box boxId
+  
+-- Input representing the active Box
+activeElement : Input Int
+activeElement = input 1
 
+-- Input representing clicks to the "Add a box" button, i.e., requests for new boxes
+newRequest : Input Bool
+newRequest = input False
+
+-- Control button to request a new box
+newBoxButton : Element
+newBoxButton = button newRequest.handle True "Add a box"
+
+-- initial list of boxes
 box1 = makeBox 1
 box2 = makeBox 2 
+
+allBoxes : [Box]
 allBoxes = [box1, box2]
+
+-- add a Box to the end of an existing list, giving it a unique ID
+addBox : [Box] -> [Box]
+addBox bxs = 
+  let maxId = foldl max 0 (map getId bxs)
+      newId = maxId + 1
+  in bxs ++ [makeBox newId]
+    
+addBoxOnClick : Bool -> [Box] -> [Box]
+addBoxOnClick cl bxs = addBox bxs
+
+-- "past dependent" boxes: a signal that updates to a new list of boxes,
+-- based on the previous list, when a request for a new box comes in
+pdBoxes : Signal [Box]
+pdBoxes = foldp addBoxOnClick allBoxes newRequest.signal
+
+-- combines the signals representing the past-dependent list of boxes and active box ID
+selectBox : Int -> [Box] -> (Int, [Box])
+selectBox id bxs = (id, bxs)
+
+activeBoxes : Signal (Int, [Box])
+activeBoxes = lift2 selectBox activeElement.signal pdBoxes
+
+-- display a scene representing the list of boxes, highlighting the active one
+displayBox : Box -> Element
+displayBox (Box id el) = el
 
 activate : Int -> Box -> Box
 activate activeId (Box id el) = if id == activeId 
   then Box id (el |> color red) 
   else Box id el
 
-displayBox : Box -> Element
-displayBox (Box id el) = el
+scene : (Int, [Box]) -> Element
+scene (activeId, bxs) = 
+  newBoxButton :: (map (displayBox . (activate activeId)) bxs) |> flow down
 
-scene : Int -> Element
-scene activeId = 
-  map (displayBox . (activate activeId)) allBoxes |> flow down
-
-main = lift scene activeElement.signal
+main = lift scene activeBoxes
