1412ee518f090dc783feddd06e1c1266ff4f200c
Richard Lawrence <richard.lawrence@berkeley.edu>
2014-08-15 10:49:39 -0700

    * Boxes.elm: now possible to dynamically update the labels of boxes
    - some code cleanup and reorganization
    - change Box representation to just include a label, not a whole Element
    - add representations for States and Events
    - add an input box to allow user to relabel currently-active box
    
    Things I learned: the limitations of foldp.
    
    1) foldp requires a pure value for initialization...so you can't
    create a past-dependent signal using an existing signal to initialize it.
    
    2) Thus, one of the necessary reorganizations was moving the foldp
    that produces the past-dependent signal representing the state of the
    list of boxes higher up in the signal dependence graph.
    
    3) I also found it necessary to introduce an Event type to encompass
    the different kinds of input the user might generate.  This allows
    merging the different kinds of input into a single signal of Events,
    which can then be the input signal to the foldp which updates the
    global state based on events.  (A unified Event type is needed because
    foldp only accepts one signal as input, whereas different user events
    produce different types of values, and the state should be updated
    whenever any one of these values changes.)

--- Boxes.elm
+++ Boxes.elm
@@ -1,14 +1,16 @@
 import Graphics.Input (Input, input, button, clickable)
+import Graphics.Input.Field (Field, field, Content, noContent, defaultStyle)
+
+-- State representation
+data State = State Int Bool Content [Box]
+
+data Event = NewID Int | NewBox Bool | NewContent Content
 
 -- Box type, constructor and accessors
-data Box = Box Int Element
+data Box = Box Int String
 
-getId (Box id el) = id
-
-makeBox boxId = asText ("Box " ++ show boxId)
-  |> container 40 50 middle
-  |> clickable activeElement.handle boxId
-  |> Box boxId
+getId (Box i s) = i
+getLabel (Box i s) = s
   
 -- Input representing the active Box
 activeElement : Input Int
@@ -22,46 +24,92 @@ newRequest = input False
 newBoxButton : Element
 newBoxButton = button newRequest.handle True "Add a box"
 
--- initial list of boxes
-box1 = makeBox 1
-box2 = makeBox 2 
+-- Input representing labels entered by user
+newLabel : Input Content
+newLabel = input noContent
 
-allBoxes : [Box]
-allBoxes = [box1, box2]
+-- Field in which user enters labels
+labelField : Signal Element
+labelField = field defaultStyle newLabel.handle id "New Label" <~ newLabel.signal
+
+relabel : Content -> Box -> Box
+relabel content (Box id lb) = Box id content.string
+
+-- initial list of boxes
+box1 = Box 1 "Box 1"
+box2 = Box 2 "Box 2"
+
+initialBoxes : [Box]
+initialBoxes = [box1, box2]
+
+boxes : Input [Box]
+boxes = input initialBoxes
 
 -- add a Box to the end of an existing list, giving it a unique ID
 addBox : [Box] -> [Box]
 addBox bxs = 
   let maxId = foldl max 0 (map getId bxs)
       newId = maxId + 1
-  in bxs ++ [makeBox newId]
-    
-addBoxOnClick : Bool -> [Box] -> [Box]
-addBoxOnClick cl bxs = addBox bxs
+  in bxs ++ [Box newId ("Box " ++ show newId)]
 
--- "past dependent" boxes: a signal that updates to a new list of boxes,
--- based on the previous list, when a request for a new box comes in
-pdBoxes : Signal [Box]
-pdBoxes = foldp addBoxOnClick allBoxes newRequest.signal
+-- relabel a selected box within an existing list
+relabelWith : Content -> Int -> [Box] -> [Box]
+relabelWith lbl activeId bxs = let 
+    isActive (Box id lbl) = activeId == id
+    maybeRelabel bx = if isActive bx then relabel lbl bx else bx
+    newBxs = map maybeRelabel bxs
+  in newBxs
 
--- combines the signals representing the past-dependent list of boxes and active box ID
-selectBox : Int -> [Box] -> (Int, [Box])
-selectBox id bxs = (id, bxs)
 
-activeBoxes : Signal (Int, [Box])
-activeBoxes = lift2 selectBox activeElement.signal pdBoxes
+-- combine the various input signals into a single signal that evolves with each change
+-- to one of the inputs
+makeState = State
+
+makeEvent : Int -> Bool -> Content -> (Int, Bool, Content)
+makeEvent = (,,)
+
+events : Signal Event
+events = merges [ NewID <~ activeElement.signal
+                , NewBox <~ newRequest.signal 
+                , NewContent <~ newLabel.signal
+                ]
+
+evolve : Event -> State -> State
+evolve ev (State activeId click oldLabel bxs) = let
+    newId = case ev of (NewID id) -> id 
+                       _ -> activeId
+    newLabel = case ev of
+      (NewContent label) -> label
+      _ -> oldLabel
+    newBxs = case ev of
+      (NewBox True) -> addBox bxs
+      (NewContent label) -> relabelWith newLabel newId bxs
+      _ -> bxs
+
+  in State newId False newLabel newBxs
+  
+states : Signal State
+states = foldp evolve (State 1 False noContent initialBoxes) events
+--states = lift evolve steps 
 
 -- display a scene representing the list of boxes, highlighting the active one
 displayBox : Box -> Element
-displayBox (Box id el) = el
+displayBox (Box id lbl) = plainText lbl 
+  |> container 100 50 middle
+  |> clickable activeElement.handle id
 
-activate : Int -> Box -> Box
-activate activeId (Box id el) = if id == activeId 
-  then Box id (el |> color red) 
-  else Box id el
+displayActiveBox : Box -> Element
+displayActiveBox = (color red) . displayBox
 
-scene : (Int, [Box]) -> Element
-scene (activeId, bxs) = 
-  newBoxButton :: (map (displayBox . (activate activeId)) bxs) |> flow down
+displayBoxes : Int -> [Box] -> [Element]
+displayBoxes activeId bxs = let 
+    isActive (Box id lbl) = activeId == id
+    disp bx = if isActive bx then displayActiveBox bx else displayBox bx
+  in map disp bxs
 
-main = lift scene activeBoxes
+scene : State -> Element -> Element
+scene (State activeId click label bxs) lblField = 
+  [newBoxButton, lblField] ++ displayBoxes activeId bxs |> flow down
+
+
+main = lift2 scene states labelField
